0cda172ab90a6a7a56e17b988b7751e152f82b6f
Ismail Nasre Eldine <ihn01@mail.aub.edu>
2018-03-13 13:41:48 +0200

    added UserManager and UserModel for basic database injections

---
+++ lostnfound/Models/EntityManager/UserManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using lostnfound.Models.DB;
+using lostnfound.Models.ViewModel;
+
+namespace lostnfound.Models.EntityManager
+{
+    public class UserManager
+    {
+        public void AddUserAccount(CreateUserView user)
+        {
+
+            using (lostfoundDB db = new lostfoundDB())
+            {
+                USER staff = new USER();
+                staff.FIRSTNAME = user.FirstName;
+                staff.LASTNAME = user.LastName;
+                staff.EMAIL = user.Email;
+                staff.PHONENUMBER = user.PhoneNumber;
+                staff.PASSWORD = user.Passowrd;
+                staff.FIRSTNAME = user.FirstName;
+                staff.ROLEID = user.RoleID;
+                db.USERs.Add(staff);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ lostnfound/Models/ViewModel/UserModel.cs
@@ -0,0 +1,107 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lostnfound.Models.ViewModel
+{
+
+
+    public class CreateUserView
+    {
+        [Key]
+
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber{ get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Password")]
+        public string Passowrd{ get; set; }
+
+        public int RoleID { get; set; }
+    }
+
+    public class CreateReporterView
+    {
+        [Key]
+
+        public int ReporterID { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        
+        public bool AUB { get; set; } 
+        public string VerificationDocument { get; set; }
+    }
+
+    public class CreateItemView
+    {
+        [Key]
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+
+        public bool AUB { get; set; }
+        public string VerificationDocument { get; set; }
+    }
+
+    public class CreateColorView
+    {
+        public int ColorID { get; set; }
+        public string Title { get; set; }
+    }
+
+
+    public class CreateLocationView
+    {
+        public int LocationID { get; set; }
+        public string Title { get; set; }
+
+    }
+
+    public class CreateCategoryView
+    {
+        public int CategoryID { get; set; }
+        public string Title { get; set; }
+    }
+}

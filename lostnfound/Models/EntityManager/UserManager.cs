c9ed60d1c9382b25d342f762d4ee8d4bdaf9e8ca
Ahmad Khalifeh <ahmad-khalifeh@outlook.com>
2018-05-08 18:19:55 +0300

    final

--- lostnfound/Models/EntityManager/UserManager.cs
+++ lostnfound/Models/EntityManager/UserManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Net;
 using System.Web.Mvc;
@@ -149,7 +150,7 @@ namespace lostnfound.Models.EntityManager
 
                 REPORTER reporter = new REPORTER
                 {
-                    FIRSTNAME = data.ReporterName,
+                    FIRSTNAME = data.FirstName,
                     LASTNAME = data.LastName,
                     EMAIL = data.Email,
                     PHONENUMBER = data.PhoneNumber,
@@ -173,7 +174,27 @@ namespace lostnfound.Models.EntityManager
                     NOTES = data.Notes
                 };
                 db.ITEMs.Add(item);
-                db.SaveChanges();
+
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbEntityValidationException ex)
+                {
+                    // Retrieve the error messages as a list of strings.
+                    var errorMessages = ex.EntityValidationErrors
+                            .SelectMany(x => x.ValidationErrors)
+                            .Select(x => x.ErrorMessage);
+
+                    // Join the list to a single string.
+                    var fullErrorMessage = string.Join("; ", errorMessages);
+
+                    // Combine the original exception message with the new one.
+                    var exceptionMessage = string.Concat(ex.Message, " The validation errors are: ", fullErrorMessage);
+
+                    // Throw a new DbEntityValidationException with the improved exception message.
+                    throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
+                }
             }
         }
 
